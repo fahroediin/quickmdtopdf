9ba147691fe66ce0f3f6614098ce2934195aed3c
Fahrudin <fahroediinj@gmail.com>
2026-06-18 15:37:28 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		allowedHosts: ['fe9edac1ac85.ngrok-free.app']
+	}
 });
